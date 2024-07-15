a3f681e641d4922c77c065e563e2b8b1896debd4
Shreejan Joshi <shreejanjoshi97@gmail.com>
2024-07-15 11:59:43 +0200

    intall all the dependency need to run payload ecommers template and seed the data

--- src/app/_components/Button/index.tsx
+++ src/app/_components/Button/index.tsx
@@ -16,6 +16,7 @@ export type Props = {
   type?: 'submit' | 'button'
   disabled?: boolean
   invert?: boolean
+  children?: React.ReactNode
 }
 
 export const Button: React.FC<Props> = ({
@@ -29,6 +30,7 @@ export const Button: React.FC<Props> = ({
   type = 'button',
   disabled,
   invert,
+  children,
 }) => {
   let el = elFromProps
 
@@ -46,6 +48,8 @@ export const Button: React.FC<Props> = ({
   const content = (
     <div className={classes.content}>
       <span className={classes.label}>{label}</span>
+      {/* in case u wanna pass some icons or labels or arrow to style our button futher */}
+      {children}
     </div>
   )
 
